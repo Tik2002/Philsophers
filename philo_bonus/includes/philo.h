3eacbba93af49f1b06b2e59544404608c99895fe
Tigran Petrosyan <tigpetro@c1r4s3.42yerevan.am>
2024-04-29 20:46:28 +0400

    Felo

---
+++ philo_bonus/includes/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tigpetro <tigpetro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/01 17:59:50 by tigpetro          #+#    #+#             */
+/*   Updated: 2024/04/29 19:42:16 by tigpetro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+// philo
+int foo(int i);
+
+#endif //PHILO_H
